9e63bc5426998f86c57dad58cb99a1cb0292b517
tylerzzheng <tylerzzheng@tencent.com>
2020-05-13 18:37:42 +0800

    9 根据评论区的提示进行优化

--- src/baseUI/horizen-item/index.js
+++ src/baseUI/horizen-item/index.js
@@ -6,7 +6,7 @@ import style from '../../assets/global-style';
 
 //样式部分
 const List = styled.div`
-  display: flex;
+  display: inline-flex; // 按评论区sharmin的修改
   align-items: center;
   height: 30px;
   overflow: hidden;
@@ -36,38 +36,23 @@ function Horizen(props) {
     const { list, oldVal, title } = props;
     const { handleClick } = props;
 
-    const Category = useRef(null)
-
-    //加入初始化内容宽度的逻辑
-    useEffect(() => {
-        let categoryDOM = Category.current
-        let tagElems = categoryDOM.querySelectorAll("span");
-        let totalWidth = 0;
-        Array.from(tagElems).forEach(ele => {
-            totalWidth += ele.offsetWidth
-        })
-        categoryDOM.style.width = `${totalWidth}px`
-    }, [list])
-
     return (
         // JSX 在Scroll下面，对第一个 div 赋予 ref
         <Scroll direction="horizental">
-            <div ref={Category}>
-                <List>
-                    <span>{title}</span>
-                    {
-                        list.map(item => (
-                            <ListItem
-                                key={item.key}
-                                className={`${oldVal === item.key ? 'selected' : ''}`}
-                                onClick={() => handleClick(item.key)}
-                            >
-                                {item.name}
-                            </ListItem>
-                        ))
-                    }
-                </List>
-            </div>
+            <List>
+                <span>{title}</span>
+                {
+                    list.map(item => (
+                        <ListItem
+                            key={item.key}
+                            className={`${oldVal === item.key ? 'selected' : ''}`}
+                            onClick={() => handleClick(item.key)}
+                        >
+                            {item.name}
+                        </ListItem>
+                    ))
+                }
+            </List>
         </Scroll>
     )
 
@@ -82,7 +67,7 @@ Horizen.defaultProps = {
 
 Horizen.propTypes = {
     list: PropTypes.array, //list 为接受的列表数据
-    oldVal: PropTypes.string,//oldVal 为当前的 item 值
+    oldVal: PropTypes.string,//oldVal 为当前的 item 值 // 我更愿意叫做selectedVal
     title: PropTypes.string,//title 为列表左边的标题
     handleClick: PropTypes.func//handleClick 为点击不同的 item 执行的方法
 };
